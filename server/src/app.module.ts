78a489f30fbf51295996c6b23c320d9ffc3da1d7
Ruslan <ruslan.mirets@gmail.com>
2022-07-24 20:08:12 +0300

    Подключение базы данных [SERVER]

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getSequelizeConfig } from './config/sequelize.config';
+import { UserModule } from './user/user.module';
+import { MovieModule } from './movie/movie.module';
+import { ReviewModule } from './review/review.module';
+import { ViewsModule } from './views/views.module';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot(),
+		SequelizeModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getSequelizeConfig,
+		}),
+		UserModule,
+		MovieModule,
+		ReviewModule,
+		ViewsModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
---
+++ server/src/config/sequelize.config.ts
@@ -0,0 +1,15 @@
+import { ConfigService } from '@nestjs/config';
+import { SequelizeModuleOptions } from '@nestjs/sequelize';
+
+export const getSequelizeConfig = async (
+	configService: ConfigService,
+): Promise<SequelizeModuleOptions> => ({
+	dialect: 'postgres',
+	host: 'localhost',
+	port: configService.get('DB_PORT'),
+	database: configService.get('DB_NAME'),
+	username: configService.get('DB_USERNAME'),
+	password: configService.get('DB_PASSWORD'),
+	autoLoadModels: true,
+	synchronize: true,
+});
